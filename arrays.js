4cbd538edd3aaa5c8e467dc15a7f0807004464f8
Kate Schwarz <kateschwarz0010@gmail.com>
2018-03-04 22:12:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,24 +2,23 @@ var chocolateBars=["snickers","hundred grand","kitkat","skittles"]
 
 function addElementToBeginningOfArray(array,element)
 {
-  var new=[element,...array];
-  return new;
+  return [element,...array]
+
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element)
 {
-  array.unshift(element);
-  return array;
+  array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(array, element)
 {
-  var new=[array,...element];
-  return new;
+  return [...array,element]
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element)
 {
-  array.push(element);
-  return array;
+  array.push(element)
+  return array
 }
